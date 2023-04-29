4990679ac26df2d3a1a392313865f9ed8e63b87d
Erica Belzer <belzer49@gmail.com>
2023-04-28 19:29:26 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code here //
+document.getElementById("main").remove();
+const newHeader=document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Erica is the champion";
+document.body.append(newHeader);
+
+ 
